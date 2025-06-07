122a5e7792490fffbdc589a364e283ca8780756b
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-06-08 03:07:47 +0600

    chore: set up jest

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  roots: ['<rootDir>/server'],
+  // The test script timeout
+  testTimeout: 30000,
+}
